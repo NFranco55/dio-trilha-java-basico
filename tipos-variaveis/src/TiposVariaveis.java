ce26c50b373f146b634300aa0c6215cc9173f5fd
franco <nazareno@lidernet.com.br>
2024-05-10 15:45:44 -0300

    alterações no código 10/05/24

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis{
     public static void main(String[] args) throws Exception {
-        System.out.println("KA-ME-HA-ME-HÁÁÁÁÁ!");
+        //System.out.println("KA-ME-HA-ME-HÁÁÁÁÁ!");
+
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+    
+        //Variáveis
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        //Contantes
+        final double VALOR_DE_PI = 3.14; // Para manter a constante com valor imutável, usar 'final' e ser escrita em caixa alta.
+    
+        System.out.println(VALOR_DE_PI);
+
+        /*Estudar
+            Tipos primitivos;
+            Classe String para representação de texto
+        */
     }
 }
