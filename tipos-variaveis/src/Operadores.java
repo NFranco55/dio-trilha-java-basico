cbcba984e7f49071332c74fd4b8aad94c2d99442
franco <nazareno@lidernet.com.br>
2024-05-13 12:27:45 -0300

    PrimeiroProjeto

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,24 @@
 public class Operadores {
     public static void main(String[] args) {
-     
-    // Operadores unários
+      /* Operadores unários
       int numero = 5;
+      numero = -numero;
+
       System.out.println(- numero);
+      System.out.println(numero);
+
+      numero = numero * -1;
+      System.out.println(numero);
+
+      */
+      // x repetição
+      
+      int numero = 5;
+        numero ++;
+
+        System.out.println(numero);
+
+
     }
 
 
