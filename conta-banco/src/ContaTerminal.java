85fe5980345c3afd380f1d8f8904576d96873e65
franco <nazareno@lidernet.com.br>
2024-05-17 15:04:36 -0300

    Codigo implementado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        // Exibir as mensagens para o nosso usuário
+        System.out.println("Digite o numero da conta: ");
+        int contaNumero = scanner.nextInt();
+
+        System.out.println("Digite a age1021ncia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo em conta: ");
+        double saldo = scanner.nextDouble();
+
+        // Obter pela scanner os valores digitados no terminal
+        System.out.println("Conta Corrente: " + contaNumero);
+        System.out.println("Agencia: " + agencia);
+        System.out.println("Nome do Cliente: " + nomeCliente);
+        System.out.println("Saldo (R$): " + saldo);
+
+
+        // Exibir a mensagem da conta criada
+
+
     }
 }
---
+++ conta-banco/src/UsuarioConta.java
@@ -0,0 +1,6 @@
+public class UsuarioConta {
+    int contaNumero = 0;
+    String agencia = " ";
+    String nomeCliente = "";
+    double saldo = 0;
+}
