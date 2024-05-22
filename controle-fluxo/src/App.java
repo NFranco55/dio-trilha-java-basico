e5d23f8379ef6eb2bb4aebda96a9bddf031cd3bd
franco <nazareno@lidernet.com.br>
2024-05-22 16:23:24 -0300

    Adição de novos arquivos

--- controle-fluxo/src/App.java
+++ controle-fluxo/src/App.java
@@ -1,5 +1,8 @@
+
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       
+       JOptionPane.showMessageDialog(null, "Ola, Mundo!");
     }
 }
--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,13 +2,16 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 10;
 
-        if(nota >= 7)
+        /*if(nota >= 7)
         System.out.println("Aprovado");
-        
-        else if(nota >=5 && nota < 7) //retorna true ou false
-        System.out.println("Em Recuperacao");
+        //retorna true ou false
+        else if(nota >=5 && nota < 7) 
+        System.out.println("Em Recuperação");
         
         else
         System.out.println("Reprovado");
+        */
+
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
     }
 }
