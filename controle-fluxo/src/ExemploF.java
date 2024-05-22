1411245392fde74793acee06df4f5b230033fe6f
franco <nazareno@lidernet.com.br>
2024-05-22 17:55:05 -0300

    Criação de novo arquivo

---
+++ controle-fluxo/src/ExemploF.java
@@ -0,0 +1,8 @@
+public class ExemploF {
+    public static void main(String[] args) {
+       for ( int carneirinhos = 1; carneirinhos <=20; carneirinhos++){
+        System.out.println("Joaozinho esta contando carneirinhos para dormir "+carneirinhos);   
+       } 
+       System.out.println("Joaozinho dormiu...");
+    }
+}
--- controle-fluxo/src/ExemploFor.java
+++
@@ -1,8 +0,0 @@
-public class ExemploFor {
-    public static void main(String[] args) {
-        for (int carneirinhos=1;carneirinhos <=20; carneirinhos++) {
-            System.out.println("contando carneirinhos "+carneirinhos);
-        }
-        System.out.println("Joaozinho dormiu...");
-    }
-}
