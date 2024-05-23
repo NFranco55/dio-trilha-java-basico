127d25510e7ab38301a8dc0dbc8744ed397d5adc
franco <nazareno@lidernet.com.br>
2024-05-23 18:41:57 -0300

    Debbug

--- controle-fluxo/src/CalculadorDeMedias.java
+++ controle-fluxo/src/CalculadorDeMedias.java
@@ -1,5 +1,23 @@
-public class ExemploDebbug {
+import java.util.Scanner;
+
+public class CalculadorDeMedias {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        double media = calculaMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %.2f", media);
+    }
+
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
+        double soma = 0;
+        for (String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextInt();
+            soma += nota;
+        }
+
+        return soma / alunos.length;
     }
 }
