30ca7f3778d98213d5ab92b5cbd3b96ea44e10f3
franco <nazareno@lidernet.com.br>
2024-05-23 13:57:02 -0300

    .

--- controle-fluxo/src/ExemploForArray.java
+++ controle-fluxo/src/ExemploForArray.java
@@ -2,8 +2,12 @@ public class ExemploForArray {
     public static void main(String[] args) {
         String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS"};
 
-        for (int x=0; x < alunos.length; x++){
+        /*for (int x=0; x < alunos.length; x++){
             System.out.println("O aluno no indice x=" + x +" eh " + alunos[x]);
+        */
+        // Forma mais agradável
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno e: "+ aluno);
         }
     }
 }
