6e15544cdc4044f238ecc701470f28cb17d6c2dc
franco <nazareno@lidernet.com.br>
2024-05-27 15:31:20 -0300

    Adicionado exemplos e o desafio

--- controle-fluxo/src/ExemploExcecao.java
+++ controle-fluxo/src/ExemploExcecao.java
@@ -6,7 +6,6 @@ public class ExemploExcecao {
    public static void main(String[] args) {
     Number valor;
     try {
-       
         valor =  NumberFormat.getInstance().parse("a1.75");
         System.out.println(valor);
             
