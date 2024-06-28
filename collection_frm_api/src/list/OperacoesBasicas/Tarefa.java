8c4936c3d5a70dee52d489f4154ee37ce87f37b9
franco <nazareno@lidernet.com.br>
2024-06-28 09:11:03 -0300

    Collections

---
+++ collection_frm_api/src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,15 @@
+package list.OperacoesBasicas;
+
+public class Terefas {
+    //atributo
+
+    private String descricao;
+
+    public Terefas(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
